7810f11d1c6908ae0afa9aad04c42d26517b5989
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-24 16:00:57 +0100

    Add basic options to security page

--- Components/Security.svelte
+++ Components/Security.svelte
@@ -1,3 +1,8 @@
+<script>
+  import SettingsOption from "$apps/Settings/Components/SettingsOption.svelte";
+  import { UserDataStore } from "$ts/stores/user";
+</script>
+
 <div class="top security-settings">
   <h2 class="title">Security Settings</h2>
   <p>
@@ -7,5 +12,23 @@
     /><br />
     We recommend leaving it on it's default settings for the best security.
   </p>
+  <hr />
+  <SettingsOption
+    title="Disable elevation"
+    context="Completely disable operations that require elevation"
+  >
+    <input type="checkbox" class="switch" bind:checked={$UserDataStore.sh.elevationDisabled} />
+  </SettingsOption>
+  <SettingsOption
+    title="Don't ask for password"
+    context="Don't require a password to approve an elevation request"
+  >
+    <input
+      type="checkbox"
+      class="switch"
+      bind:checked={$UserDataStore.sh.securityNoPassword}
+      disabled={$UserDataStore.sh.elevationDisabled}
+    />
+  </SettingsOption>
 </div>
 <div class="toolbar" />
