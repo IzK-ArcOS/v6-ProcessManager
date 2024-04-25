c23f6afc690d39959f9f1ff9f5d86c5635fda71f
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-04-25 11:05:31 +0200

    Move Security to Settings

--- App.svelte
+++ App.svelte
@@ -4,7 +4,6 @@
   import { ProcessStack } from "$ts/stores/process";
   import { onMount } from "svelte";
   import Processes from "./Components/Processes.svelte";
-  import Security from "./Components/Security.svelte";
   import Services from "./Components/Services.svelte";
   import Users from "./Components/Users.svelte";
   import "./css/main.css";
@@ -27,12 +26,11 @@
   }
 </script>
 
-<Tabs tabs={["Processes", "Services", "Users", "Security"]} bind:current>
+<Tabs tabs={["Processes", "Services", "Users"]} bind:current>
   <div class="renderer" slot="renderer">
     <Tab {current} selector="Processes"><Processes {runtime} /></Tab>
     <Tab {current} selector="Services"><Services {runtime} bind:current /></Tab>
     <Tab {current} selector="Users"><Users /></Tab>
-    <Tab {current} selector="Security"><Security /></Tab>
   </div>
   <button class="close material-icons-round" slot="right" on:click={closeThis}>close</button>
 </Tabs>
